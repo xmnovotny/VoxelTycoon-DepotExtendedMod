554dcea8b85955594020d4888e3e2f9a4914c3a4
Michal Novotny <xmnovotny@gmail.com>
2021-08-19 23:54:13 +0200

    A whole train enters the depot

--- DepotExtended/GoToDepot/GoToDepotManager.cs
+++ DepotExtended/GoToDepot/GoToDepotManager.cs
@@ -6,7 +6,10 @@ using System.Reflection.Emit;
 using HarmonyLib;
 using JetBrains.Annotations;
 using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
 using VoxelTycoon;
+using VoxelTycoon.Game.UI;
 using VoxelTycoon.Notifications;
 using VoxelTycoon.Serialization;
 using VoxelTycoon.Tracks;
@@ -18,15 +21,21 @@ using XMNUtils;
 namespace DepotExtended.GoToDepot
 {
     [HarmonyPatch]
-    [SchemaVersion(1)]
+    [SchemaVersion(2)]
     public class GoToDepotManager: SimpleManager<GoToDepotManager>
     {
         private readonly Dictionary<Vehicle, GoToDepotVehicleData> _vehiclesToDepot = new();
+        private readonly HashSet<Vehicle> _trainEnteringDepot = new ();
+        private Action<TrackUnit,bool> _trainUpdatePosition;
+        private Action<TrackUnit, TrackPosition> _setTrainFrontBound;
+        private Func<TrackUnit, List<TrackUnitPoint>> _trainPointsGetter;
+        private Action<TrackUnit, float> _setTrainLength;
+        private Action<Vehicle> _trainUpdateLengthAndPoints;
 
         private GoToDepotVehicleData GoToDepot(VehicleDepot depot, [NotNull] GoToDepotOverrideTask task)
         {
             if (task == null) throw new ArgumentNullException(nameof(task));
-            return _vehiclesToDepot[task.Vehicle] = new GoToDepotVehicleData(depot, task);
+            return _vehiclesToDepot[task.Vehicle] = new GoToDepotVehicleData(task.Vehicle, depot, task);
         }
 
         [UsedImplicitly]
@@ -49,11 +58,23 @@ namespace DepotExtended.GoToDepot
 
         private IEnumerator RunGoToDepotTask(GoToDepotOverrideTask __instance, IEnumerator origEnumerator)
         {
-            GoToDepotVehicleData data;
-            while ((data = _vehiclesToDepot.GetValueOrDefault(__instance.Vehicle)) != null && __instance.Vehicle.FrontBound.Connection != data.TargetConnection)
+            GoToDepotVehicleData data = _vehiclesToDepot.GetValueOrDefault(__instance.Vehicle);
+            ReusableWaitForSeconds wfs = new ReusableWaitForSeconds();
+            if (data != null)
             {
-                yield return new WaitForSeconds(1);
+                while (__instance.Vehicle.FrontBound.Connection != data.TargetConnection)
+                {
+                    yield return wfs.Wait(0.1f);
+                }
             }
+
+            if (__instance.Vehicle is Train train)
+            {
+                _trainEnteringDepot.Add(train);
+                while (_trainEnteringDepot.Contains(train))
+                    yield return wfs.Wait(0.1f);
+            }
+
             //remove data, they are no longer needed
             _vehiclesToDepot.Remove(__instance.Vehicle);
             yield return origEnumerator;
@@ -81,6 +102,8 @@ namespace DepotExtended.GoToDepot
                     }
                 }
             }
+
+            ReadEnteringTrains(reader);
         }
 
         internal void Write(StateBinaryWriter writer)
@@ -88,34 +111,127 @@ namespace DepotExtended.GoToDepot
             using PooledList<KeyValuePair<Vehicle,GoToDepotVehicleData>> toSave = PooledList<KeyValuePair<Vehicle,GoToDepotVehicleData>>.Take();
             foreach (KeyValuePair<Vehicle,GoToDepotVehicleData> pair in _vehiclesToDepot)
             {
+                //save only when there is turnaround task and vehicle is moving to the depot
                 if (pair.Key.Schedule.OverrideTask is TurnAroundOverrideTask turnTask && pair.Value.TurningTask == turnTask) 
                     toSave.Add(pair);
             }
             writer.WriteInt(toSave.Count);
-            if (toSave.Count == 0)
-                return;
-
-            MethodInfo mInf = AccessTools.Method(typeof(GoToDepotOverrideTask), "Write");
-            foreach (KeyValuePair<Vehicle,GoToDepotVehicleData> pair in toSave)
+            if (toSave.Count != 0)
             {
-                writer.WriteInt(pair.Key.Id); //vehicle
-                writer.WriteBuilding(pair.Value.Depot);
-                mInf.Invoke(pair.Value.Task, new object[] {writer});
+                MethodInfo mInf = AccessTools.Method(typeof(GoToDepotOverrideTask), "Write");
+                foreach (KeyValuePair<Vehicle, GoToDepotVehicleData> pair in toSave)
+                {
+                    GoToDepotVehicleData data = pair.Value;
+                    writer.WriteInt(pair.Key.Id); //vehicle
+                    writer.WriteBuilding(data.Depot);
+                    mInf.Invoke(data.Task, new object[] {writer});
+                }
+
+            }
+
+            //write data of trains entering depot
+            WriteEnteringTrains(writer);
+        }
+
+        internal void TrainUpdatePosition(Train train)
+        {
+            if (_trainUpdatePosition == null)
+            {
+                MethodInfo mInf = typeof(TrackUnit).GetMethod("UpdatePosition", BindingFlags.NonPublic | BindingFlags.Instance);
+                _trainUpdatePosition = (Action<TrackUnit, bool>)Delegate.CreateDelegate(typeof(Action<TrackUnit, bool>), mInf);
+            }
+
+            _trainUpdatePosition(train, true);
+        }
+
+        internal void SetTrainFrontBound(Train train, TrackPosition position)
+        {
+            if (_setTrainFrontBound == null)
+            {
+                MethodInfo mInf = typeof(TrackUnit).GetMethod("set_FrontBound", BindingFlags.NonPublic | BindingFlags.Instance);
+                _setTrainFrontBound = (Action<TrackUnit, TrackPosition>)Delegate.CreateDelegate(typeof(Action<TrackUnit, TrackPosition>), mInf);
+            }
+
+            _setTrainFrontBound(train, position);
+        }
+
+        internal void SetTrainLength(Train train, float length)
+        {
+            if (_setTrainLength == null)
+            {
+                MethodInfo mInf = typeof(TrackUnit).GetMethod("set_Length", BindingFlags.NonPublic | BindingFlags.Instance);
+                _setTrainLength = (Action<TrackUnit, float>)Delegate.CreateDelegate(typeof(Action<TrackUnit, float>), mInf);
+            }
+
+            _setTrainLength(train, length);
+        }
+
+        internal void TrainUpdateLengthAndPoints(Train train)
+        {
+            if (_trainUpdateLengthAndPoints == null)
+            {
+                MethodInfo mInf = typeof(Vehicle).GetMethod("UpdateLengthAndPoints", BindingFlags.NonPublic | BindingFlags.Instance);
+                _trainUpdateLengthAndPoints = (Action<Vehicle>)Delegate.CreateDelegate(typeof(Action<Vehicle>), mInf);
+            }
+
+            _trainUpdateLengthAndPoints(train);
+        }
+
+        internal List<TrackUnitPoint> GetVehiclePoints(Vehicle train)
+        {
+            if (_trainPointsGetter == null)
+                _trainPointsGetter = SimpleDelegateFactory.FieldGet<TrackUnit, List<TrackUnitPoint>>("Points");
+
+            return _trainPointsGetter.Invoke(train);
+        }
+
+        private void ReadEnteringTrains(StateBinaryReader reader)
+        {
+            if (!SchemaVersion<GoToDepotManager>.AtLeast(2))
+                return;
+            int enteringCount = reader.ReadInt();
+            for (int i = 0; i < enteringCount; i++)
+            {
+                int vehicleID = reader.ReadInt();
+                if (vehicleID > 0)
+                {
+                    Vehicle vehicle = LazyManager<VehicleManager>.Current.FindById(vehicleID);
+                    _trainEnteringDepot.Add(vehicle);
+                    GoToDepotVehicleData data = _vehiclesToDepot[vehicle];
+                    data.ReadEnteringData(reader);
+                }
             }
         }
-        
-#region HARMONY
+
+        private void WriteEnteringTrains(StateBinaryWriter writer)
+        {
+            writer.WriteInt(_trainEnteringDepot.Count);
+            foreach (Vehicle vehicle in _trainEnteringDepot)
+            {
+                if (_vehiclesToDepot.TryGetValue(vehicle, out GoToDepotVehicleData data))
+                {
+                    writer.WriteInt(vehicle.Id);
+                    data.WriteEnteringData(writer);
+                }
+                else
+                    writer.WriteInt(0); //invalid data
+            }
+        }
+
+        #region HARMONY
         [HarmonyPostfix]
         [HarmonyPatch(typeof(GoToDepotDelayedAction), "Initialize")]
+        [UsedImplicitly]
         // ReSharper disable once InconsistentNaming
-        private static void GoToDepotDelayedAction_Initialize_pof(ref float ____delay)
+        private static void GoToDepotDelayedAction_Initialize_pof(VehicleDepot depot, ref float ____delay)
         {
             if (Current != null)
-                ____delay = 1f;
+                ____delay = depot is RailDepot ? 0.1f : 1f;
         }
 
         [HarmonyPostfix]
         [HarmonyPatch(typeof(GoToDepotOverrideTask), "Initialize")]
+        [UsedImplicitly]
         // ReSharper disable once InconsistentNaming
         private static void GoToDepotOverrideTask_Initialize_pof(GoToDepotOverrideTask __instance, VehicleDepot depot)
         {
@@ -124,6 +240,7 @@ namespace DepotExtended.GoToDepot
 
         [HarmonyPostfix]
         [HarmonyPatch(typeof(GoToDepotOverrideTask), "Read")]
+        [UsedImplicitly]
         // ReSharper disable once InconsistentNaming
         private static void GoToDepotOverrideTask_Read_pof(GoToDepotOverrideTask __instance, GoToDepotDelayedAction ____action)
         {
@@ -137,6 +254,7 @@ namespace DepotExtended.GoToDepot
         
         [HarmonyPostfix]
         [HarmonyPatch(typeof(GoToDepotOverrideTask), "Run")]
+        [UsedImplicitly]
         // ReSharper disable once InconsistentNaming
         private static void GoToDepotOverrideTask_Run_pof(GoToDepotOverrideTask __instance, ref IEnumerator __result)
         {
@@ -148,6 +266,7 @@ namespace DepotExtended.GoToDepot
 
         [HarmonyPrefix]
         [HarmonyPatch(typeof(VehicleSchedule), "PushOverrideTask")]
+        [UsedImplicitly]
         // ReSharper disable once InconsistentNaming
         private static void GoToDepotOverrideTask_PushOverrideTask_prf(VehicleSchedule __instance, OverrideTask task)
         {
@@ -171,6 +290,7 @@ namespace DepotExtended.GoToDepot
         
         [HarmonyPostfix]
         [HarmonyPatch(typeof(VehicleTask), "OnCompleted")]
+        [UsedImplicitly]
         // ReSharper disable once InconsistentNaming
         private static void VehicleTask_OnCompleted_pof(VehicleTask __instance)
         {
@@ -193,6 +313,7 @@ namespace DepotExtended.GoToDepot
 
         [HarmonyPostfix]
         [HarmonyPatch(typeof(GoToDepotDelayedAction), "DoDelayedAction")]
+        [UsedImplicitly]
         // ReSharper disable once InconsistentNaming`
         private static void GoToDepotDelayedAction_DoDelayedAction_pof(GoToDepotDelayedAction __instance, Vehicle ____vehicle, VehicleDepot ____depot)
         {
@@ -201,11 +322,17 @@ namespace DepotExtended.GoToDepot
                 //TODO: Add translation
                 Manager<NotificationManager>.Current.Push($"Vehicle arrived to the depot", $"{____vehicle.Name} just arrived to the {____depot.Name}", 
                     new GoToVehicleNotificationAction(____vehicle));
+                //restore original train points
+                if (____vehicle is Train train)
+                {
+                    Current.TrainUpdateLengthAndPoints(train);                    
+                }
             }
         }
 
         [HarmonyPostfix]
         [HarmonyPatch(typeof(TrackPathNodeManager<RoadPathNodeManager, RoadPathNode>), "CanPass")]
+        [UsedImplicitly]
         // ReSharper disable once InconsistentNaming
         private static void RoadPathNodeManager_CanPass_pof(RoadPathNodeManager __instance, ref bool __result, TrackConnection connection1)
         {
@@ -218,6 +345,7 @@ namespace DepotExtended.GoToDepot
 
         [HarmonyTranspiler]
         [HarmonyPatch(typeof(Vehicle), "UpdateDestination")]
+        [UsedImplicitly]
         private static IEnumerable<CodeInstruction> Vehicle_UpdateDestination_trans(IEnumerable<CodeInstruction> instructions)
         {
             bool found = false;
@@ -242,6 +370,79 @@ namespace DepotExtended.GoToDepot
                 yield return instruction;
             }
         }
+        
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(Vehicle), "Move")]
+        [UsedImplicitly]
+        // ReSharper disable once InconsistentNaming
+        private static bool Vehicle_Move_pof(Vehicle __instance)
+        {
+            if (__instance is Train train && Current?._trainEnteringDepot.Count>0 && Current._trainEnteringDepot.Contains(__instance))
+            {
+                if (!Current._vehiclesToDepot.TryGetValue(train, out GoToDepotVehicleData data))
+                {
+                    Current._trainEnteringDepot.Remove(train);
+                    return true;
+                }
+
+                if (data.MoveTrainInDepot())
+                {
+                    //done moving train in the depot
+                    Current._trainEnteringDepot.Remove(train);
+                }
+                return false;
+            }
+            return true;
+        }
+
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(VehicleWindowHeaderView), "Update")]
+        [UsedImplicitly]
+        // ReSharper disable once InconsistentNaming`
+        private static void VehicleWindowHeaderView_Update_pof(VehicleWindowHeaderView __instance, Vehicle ____vehicle)
+        {
+            if (Current != null && ____vehicle is Train {IsWrecked: false, IsInDepot: false} train)
+            {
+                //test if we are completely out of the depot
+                if (train.RearBound.Position == 0f && train.RearBound.Connection.OuterConnectionCount == 0 && train.RearBound.Connection.Track.Parent is RailDepot  //train is leaving depot 
+                 || train.Schedule.OverrideTask is GoToDepotOverrideTask && train.FrontBound.Position >= 1f && train.FrontBound.Connection.InnerConnection.OuterConnectionCount == 0 && train.FrontBound.Connection.Track.Parent is RailDepot)  //train is entering depot
+                {
+                    //train is leaving depot
+                    __instance.GoHomeButton.interactable = false;
+                    __instance.TurnAroundButton.interactable = false;
+/*                    if (train.Schedule.OverrideTask is GoToDepotOverrideTask)
+                        __instance.OnOffToggle.Toggle.interactable = false;*/
+                }
+            }
+        }
+
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(VehicleEnabledToggle), "Initialize")]
+        [UsedImplicitly]
+        // ReSharper disable once InconsistentNaming`
+        private static void VehicleEnabledToggle_Initialize_pof(VehicleEnabledToggle __instance, Vehicle vehicle)
+        {
+            Toggle.ToggleEvent newEvent = new Toggle.ToggleEvent();
+            var origEvent = __instance.Toggle.onValueChanged;
+            newEvent.AddListener(delegate (bool value)
+                {
+                    if (!value && vehicle is Train {IsWrecked: false, IsInDepot: false} train)
+                    {
+                        //test if we are completely out of the depot
+                        if (train.Schedule.OverrideTask is GoToDepotOverrideTask && train.FrontBound.Position >= 1f && train.FrontBound.Connection.InnerConnection.OuterConnectionCount == 0 &&
+                            train.FrontBound.Connection.Track.Parent is RailDepot) //train is entering depot
+                        {
+                            //disable event
+                            return;
+                        }
+                    }
+                    origEvent.Invoke(value);
+                }
+            );
+            __instance.Toggle.onValueChanged = newEvent;
+        }
+
+       
 #endregion
     }
 }
--- DepotExtended/GoToDepot/GoToDepotVehicleData.cs
+++ DepotExtended/GoToDepot/GoToDepotVehicleData.cs
@@ -1,15 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using HarmonyLib;
 using UnityEngine;
+using VoxelTycoon.Serialization;
 using VoxelTycoon.Tracks;
 using VoxelTycoon.Tracks.Rails;
 using VoxelTycoon.Tracks.Roads;
 using VoxelTycoon.Tracks.Tasks;
+using XMNUtils;
 
 namespace DepotExtended.GoToDepot
 {
+    [SchemaVersion(1)]
     public class GoToDepotVehicleData: IVehicleDestination
     {
+        private readonly Vehicle _vehicle;
+        private float _distanceBeyondEnd;
+        private float _enterVelocity = float.MinValue;
+        private List<TrackUnitPoint> _trainPoints;
         public string Name { get; }
 
         public HashSet<TrackConnection> Stops { get; } = new();
@@ -19,9 +27,30 @@ namespace DepotExtended.GoToDepot
         public TurnAroundOverrideTask TurningTask { get; set; }
         public GoToDepotOverrideTask Task { get; }
         public VehicleDepot Depot { get; }
+        public bool IsTrainEnteringDepot => _distanceBeyondEnd > 0; 
 
-        public GoToDepotVehicleData(VehicleDepot depot, GoToDepotOverrideTask task)
+        internal void WriteEnteringData(StateBinaryWriter writer)
         {
+            //saving only when trains are entering depot
+            writer.WriteFloat(_distanceBeyondEnd);
+            writer.WriteFloat(_enterVelocity);
+        }
+
+        internal void ReadEnteringData(StateBinaryReader reader)
+        {
+            _distanceBeyondEnd = reader.ReadFloat();
+            _enterVelocity = reader.ReadFloat();
+            _vehicle.Velocity = _enterVelocity;
+            if (_distanceBeyondEnd > 0f)
+            {
+                UpdateTrainPoints(_vehicle.Length, _distanceBeyondEnd);
+                SimpleManager<GoToDepotManager>.Current!.TrainUpdatePosition(_vehicle as Train);
+            }
+        }
+
+        public GoToDepotVehicleData(Vehicle vehicle, VehicleDepot depot, GoToDepotOverrideTask task)
+        {
+            _vehicle = vehicle;
             Depot = depot ? depot : throw new ArgumentNullException(nameof(depot));
             Target = depot.Center;
             Name = depot.Name;
@@ -42,6 +71,95 @@ namespace DepotExtended.GoToDepot
             }
         }
 
+        /** true if moving is done */
+        public bool MoveTrainInDepot()
+        {
+            if (_vehicle is not Train train)
+                return true;
+            // ReSharper disable once CompareOfFloatsByEqualityOperator
+            if (_enterVelocity == float.MinValue)
+                _enterVelocity = Math.Max(train.Velocity, 2f);
+
+            train.Velocity = _enterVelocity;
+            float distance = ScaleDistance(train.Velocity) * Time.smoothDeltaTime;
+            GoToDepotManager goToDepotManager = SimpleManager<GoToDepotManager>.Current;
+            if (train.FrontBound.Position < 1f)
+            {
+                //normal travel towards end of the track
+                float connLength = train.FrontBound.Connection.Length;
+                float distToEnd = connLength * (1f - train.FrontBound.Position);
+                TrackPosition frontBound = train.FrontBound;
+                if (distance > distToEnd)
+                {
+                    frontBound.Position = 1f;
+                    distance -= distToEnd;
+                }
+                else
+                {
+                    frontBound.Position += distance / connLength;
+                    distance = 0f;
+                }
+                goToDepotManager!.SetTrainFrontBound(train, frontBound);
+            }
+
+            _distanceBeyondEnd += distance;
+            float origLength = train.Length;
+            if (distance > 0f)
+            {
+                //travel beyond end of the track
+                UpdateTrainPoints(origLength, distance);
+            }
+
+            if (_distanceBeyondEnd > 0f)
+            {
+                goToDepotManager!.SetTrainLength(train, Math.Max(1f, origLength - _distanceBeyondEnd));
+            }
+            goToDepotManager!.TrainUpdatePosition(train);
+            if (_distanceBeyondEnd > 0f)
+            {
+                goToDepotManager!.SetTrainLength(train, origLength);
+            }
+            if (train.RearBound.Connection == train.FrontBound.Connection)
+            {
+                //all train is in the depot, stop executing own moving
+                return true;
+            }
+
+            return false;
+        }
+
+        private void UpdateTrainPoints(float origLength, float distance)
+        {
+            bool flipped = _vehicle.Flipped;
+            if (_trainPoints == null)
+                LoadTrainPoints();
+            for (int i = _trainPoints.Count - 1; i >= 0; i--)
+            {
+                TrackUnitPoint point = _trainPoints[i];
+                bool remove;
+                if (flipped)
+                {
+                    remove = point.Offset > origLength - _distanceBeyondEnd;
+                }
+                else
+                {
+                    point.Offset -= distance;
+                    remove = point.Offset < 0f;
+                }
+
+                if (remove)
+                {
+                    point.Clamped = true;
+                    _trainPoints.RemoveAt(i);
+                }
+            }
+        }
+
+        private void LoadTrainPoints()
+        {
+            _trainPoints = SimpleManager<GoToDepotManager>.Current!.GetVehiclePoints(_vehicle);
+        }
+        
         private TrackConnection FindEndConnection(TrackConnection spawnConnection)
         {
             TrackConnection conn = spawnConnection;
@@ -54,5 +172,11 @@ namespace DepotExtended.GoToDepot
                 conn = conn.OuterConnections[0].InnerConnection;
             return conn.InnerConnection;
         }
+        
+        private float ScaleDistance(float distance)
+        {
+            return distance / 5f;
+        }
+
     }
 }
