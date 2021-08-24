6739eb3c05c0c3d9cb2d9069a1972b49b66f052b
Michal Novotny <xmnovotny@gmail.com>
2021-08-24 15:35:49 +0200

    Fix for wrong slope force calculation when vehicle is flipped

---
+++ DepotExtended/SlopeFix.cs
@@ -0,0 +1,32 @@
+﻿using HarmonyLib;
+using JetBrains.Annotations;
+using UnityEngine;
+using VoxelTycoon.Tracks;
+
+namespace DepotExtended
+{
+    [HarmonyPatch]
+    public static class SlopeFix
+    {
+        [UsedImplicitly]
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(Vehicle), "GetSlopeForce")]
+        // ReSharper disable InconsistentNaming
+        private static bool Vehicle_GetSlopeForce_prf(Vehicle __instance, bool useMaxWeight, out float __result)
+        {
+            float num = 0f;
+            int unitsCount = __instance.Units.Count;
+            float flipped = __instance.Flipped ? -1 : 1;
+            for (int i = 0; i < unitsCount; i++)
+            {
+                VehicleUnit vehicleUnit = __instance.Units[i];
+                float num2 = Vector3.Dot(vehicleUnit.transform.forward, Vector3.down) * (5f / 26f);
+                float num3 = (useMaxWeight ? vehicleUnit.MaxWeight : vehicleUnit.Weight) * num2 * flipped;
+                num += num3;
+            }
+            __result = num * 9.8f;
+            return false;
+        }
+
+    }
+}
